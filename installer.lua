405d32d2374c0434d218b996bbb8f51d8ba7ee3f
TuShaggy <122480537+TuShaggy@users.noreply.github.com>
2025-09-15 21:30:18 +0200

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -1,8 +1,10 @@
-
+---------------------------
+-- FILE: installer.lua
+---------------------------
 -- Optional self-updater. Replace RAW_BASE with your repo raw URL.
 -- Example: https://raw.githubusercontent.com/TuShaggy/posta/main
 
-local RAW_BASE = "https://github.com/TuShaggy/CC-Draconic"
+local RAW_BASE = "https://raw.githubusercontent.com/TuShaggy/posta/main"
 local FILES = { "startup.lua", "lib/f.lua" }
 
 local function fetch(url)
